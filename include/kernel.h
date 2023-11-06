8fd3caeb6547374f60ae6bdace6d7265cb105686
RolandGamos <lucasweglarz9@gmail.com>
2023-11-06 14:08:57 +0100

    proper screen interface

--- include/kernel.h
+++ include/kernel.h
@@ -2,25 +2,4 @@
 # define KERNEL_H
 
 
-# define VGA_TEXT(COLOR, BRIGHT, ASCII) ((COLOR + ((BRIGHT == 1) ? 8 : 0)) << FORE_COLOUR_OFFSET | ASCII)
-
-/* Screen offset*/
-# define VGA_AREA 0xb8000
-
-# define FORE_COLOUR_OFFSET 8
-# define BACKGROUND_COLOUR_OFFSET 12
-
-/* colors*/
-# define BLACK_COLOUR (0x0)
-# define BLUE_COLOUR (0x1)
-# define GREEN_COLOUR (0x2)
-# define CYAN_COLOUR (0x3)
-# define RED_COLOUR (0x4)
-# define RED_COLOUR (0x4)
-# define MAGENTA_COLOUR (0x5)
-# define BROWN_COLOUR (0x6)
-# define LIGHT_GRAY_COLOUR (0x7)
-
-
-
 #endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
-#include <kernel.h>
+#include <vga.h>
 
-void print_letter(char c){
-    short letter = VGA_TEXT(BLUE_COLOUR,  0, c);
-    *((unsigned int*)VGA_AREA) = letter;
+unsigned short *vga_area_head = ((unsigned short*)VGA_AREA);
 
-    // We can add background color this way
-    //*((unsigned int*)VGA_AREA) = (0x4 << 12) | letter;
+void print_ascii(char c){
+    if (vga_area_head == (VGA_AREA + VGA_TEXT_MODE_SIZE))
+        return ;
+    *vga_area_head = VGA_ASCII(BLUE_COLOUR, 0, c);
+    vga_area_head++;
+ }
+
+ void clear_screen(){
+    while (vga_area_head != ((unsigned short*)VGA_AREA)){
+        *vga_area_head = VGA_BG(BLACK_COLOUR, 0);
+        vga_area_head--;
+    }
+    *vga_area_head = VGA_BG(BLACK_COLOUR, 0);
+    vga_area_head--;
  }
 
 int main(void){
-    print_letter('C');
+    print_ascii('4');
+    print_ascii('2');
     while(1){
     }
 }
