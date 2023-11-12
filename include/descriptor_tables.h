3eaf73e99ba292f25c5b51b3850bebdd8573f009
bsteve456 <stevealan1@protonmail.com>
2023-11-12 21:13:02 +0100

    add GDT

--- include/descriptor_tables.h
+++ include/descriptor_tables.h
@@ -3,19 +3,19 @@
 
 struct gdt_entry_struct
 {
-   u16int limit_low;           
-   u16int base_low;            
-   u8int  base_middle;         
-   u8int  access;             
-   u8int  granularity;
-   u8int  base_high;          
+   unsigned short limit_low;           
+   unsigned short base_low;            
+   unsigned char  base_middle;         
+   unsigned char  access;             
+   unsigned char  flags;
+   unsigned char  base_high;          
 } __attribute__((packed));
 typedef struct gdt_entry_struct gdt_entry_t;
 
 struct gdt_ptr_struct
 {
-   u16int limit;              
-   u32int base;            
+   unsigned short limit;              
+   unsigned long  base;            
 }
  __attribute__((packed));
 typedef struct gdt_ptr_struct gdt_ptr_t;
--- src/boot.s
+++ src/boot.s
@@ -14,6 +14,21 @@ section .text:
 extern main
 
 global start_kernel
+global gdt_flush
 
 start_kernel:
   call main
+
+gdt_flush:
+   mov eax, [esp+4]  
+   lgdt [eax]        
+
+   mov ax, 0x10      
+   mov ds, ax       
+   mov es, ax
+   mov fs, ax
+   mov gs, ax
+   mov ss, ax
+   jmp 0x08:.flush   
+.flush:
+   ret
