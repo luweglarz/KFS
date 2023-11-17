ff961aeca3f548e3ba1497f7549363d33f5b6dd4
stbaleba <stevealan1@protonmail.com>
2023-11-17 17:19:04 +0100

    keyboard working

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,7 +3,10 @@
 #include "descriptor_tables.h"
 #include "isr.h"
 
-unsigned char kbdus[128] =
+int CapsLock = 0;
+int CapsLockStatus = 1;
+
+unsigned char lowercase[128] =
 {
 	0,  0, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
 	'9', '0', '-', '=', '\b',	/* Backspace */
@@ -43,7 +46,42 @@ unsigned char kbdus[128] =
 	0,	/* All other keys are undefined */
 };		
 
-
+unsigned char uppercase[128] =
+{
+	0,  0, '!','@','#','$','%','^','&','*','(',')','_','+','\b','\t',			/* Tab */
+	'Q','W','E','R','T','Y','U','I','O','P','{','}','\n'
+,	/* Enter key */
+	0,			/* 29   - Control */
+	'A','S','D','F','G','H','J','K','L',':','"','~',0,	/* 39 */
+	 		/* Left shift */
+	'|','Z','X','C', 'V','B','N','M','<','>','?',0,				/* Right shift */
+	'*',
+	0,	/* Alt */
+	' ',	/* Space bar */
+	0,	/* Caps lock */
+	0,	/* 59 - F1 key ... > */
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,	/* < ... F10 */
+	0,	/* 69 - Num lock*/
+	0,	/* Scroll Lock */
+	0,	/* Home key */
+	0,	/* Up Arrow */
+	0,	/* Page Up */
+	'-',
+	0,	/* Left Arrow */
+	0,
+	0,	/* Right Arrow */
+	'+',
+	0,	/* 79 - End key*/
+	0,	/* Down Arrow */
+	0,	/* Page Down */
+	0,	/* Insert Key */
+	0,	/* Delete Key */
+	0,   0,   0,
+	0,	/* F11 Key */
+	0,	/* F12 Key */
+	0,	/* All other keys are undefined */
+};
 
 void keyboardHandler(registers_t regs)
 {
@@ -54,8 +92,27 @@ void keyboardHandler(registers_t regs)
 	}
 	else
 	{
-		if(kbdus[scanCode] != 0 && kbdus[scanCode] != '\b')
-			kputchar(kbdus[scanCode]);
+		if (scanCode == 0x3A)
+		{	
+			if(CapsLockStatus == 1)
+				CapsLock += 1;
+			else
+				CapsLock -= 1;
+		}
+		if(CapsLock == 2 || CapsLock == 0)
+		{
+			if (CapsLock == 2)
+				CapsLockStatus = 0;
+			else
+				CapsLockStatus = 1;
+		}
+		if (lowercase[scanCode] != 0 && lowercase[scanCode] != '\b' && lowercase[scanCode] != '\n')
+		{
+			if(CapsLock == 2)
+				kputchar(uppercase[scanCode]);
+			else
+				kputchar(lowercase[scanCode]);
+		}
 	}
 }
 
