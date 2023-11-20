7b7be3935d7cc5c4a0b8db200ffe883d9404c24e
bsteve456 <stevealan1@protonmail.com>
2023-11-20 20:45:39 +0100

    simplify code for GDT IDT

--- src/descriptor_tables.c
+++ src/descriptor_tables.c
@@ -31,11 +31,8 @@ static void init_gdt()
 	gdt_ptr.limit = (sizeof(gdt_entry_t) * 5) - 1;
 	gdt_ptr.base  = (unsigned long)&gdt_entries;
 
-	gdt_set_gate(0, 0, 0, 0, 0);                
 	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); 
 	gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
-	gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
-	gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
 
 	gdt_flush((unsigned long)&gdt_ptr);
 }
