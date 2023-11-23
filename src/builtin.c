daf7d068e89241de56cf9bc71b6c21c55e1f8f62
RolandGamos <lucasweglarz9@gmail.com>
2023-11-23 16:44:04 +0100

    reboot command

--- src/builtin.c
+++ src/builtin.c
@@ -1,3 +1,11 @@
-int reboot(){
-
+#include "klib.h"
+ 
+int reboot()
+{
+    /* 
+        0x64 :  PS/2 command register port 
+        0xFE :  reset CPU command 
+    */
+    outb(0x64, 0xFE);
+    return (0);
 }
